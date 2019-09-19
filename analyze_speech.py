86df136d06711d7ab4c7a4ae5c44b6aeb15f028c
Amadeus Magrabi <amadeus.magrabi@gmail.com>
2019-09-19 16:04:23 +0200

    Transcript and speaker ratio as json output

--- analyze_speech.py
+++ analyze_speech.py
@@ -1,62 +1,21 @@
-import io
 import os
-# import ffmpeg
+import subprocess
+import pprint
 
+from pydub import AudioSegment
 from google.oauth2 import service_account
 
 CREDENTIALS = service_account.Credentials.from_service_account_file('ct-machine-learning-6795b2605267.json')
 
 
-# def convert_audio(file_path, output_type="flac"):
-#     from pydub import AudioSegment
-#     song = AudioSegment.from_wav(file_path)
-#     song.export(f'{file_path}.{output_type}', format=output_type)
-
-def sample_long_running_recognize(storage_uri):
-    """
-    Transcribe long audio file from Cloud Storage using asynchronous speech
-    recognition
-
-    Args:
-      storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
-    """
-    from google.cloud import speech_v1
-    from google.cloud.speech_v1 import enums
-
-    storage_uri = "gs://cthack19-meeting-moderator-machine/audio_samples/meeting_business_01_6mins.mp3"
-
-    client = speech_v1.SpeechClient(credentials=CREDENTIALS)
-
-    # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
-
-    # Sample rate in Hertz of the audio data sent
-    sample_rate_hertz = 16000
-
-    # The language of the supplied audio
-    language_code = "en-US"
-
-    # Encoding of audio data sent. This sample sets this explicitly.
-    # This field is optional for FLAC and WAV audio formats.
-    encoding = enums.RecognitionConfig.AudioEncoding.FLAC
-    config = {
-        "sample_rate_hertz": sample_rate_hertz,
-        "language_code": language_code,
-        "encoding": encoding,
-    }
-    audio = {"uri": storage_uri}
-
-    operation = client.long_running_recognize(config, audio)
-
-    print(u"Waiting for operation to complete...")
-    response = operation.result()
-
-    for result in response.results:
-        # First alternative is the most probable result
-        alternative = result.alternatives[0]
-        print(u"Transcript: {}".format(alternative.transcript))
+def convert_ogg2flac(file_path):
+    audio = AudioSegment.from_ogg(file_path)
+    new_file_path = file_path.split(".")[0] + ".flac"
+    audio.export(new_file_path, format="flac")
+    return new_file_path
 
 
-def analyze_with_speaker_recog(local_file_path):
+def analyze_with_speaker_recog(file_path):
     """
     Print confidence level for individual words in a transcription of a short audio
     file
@@ -69,42 +28,74 @@ def analyze_with_speaker_recog(local_file_path):
     from google.cloud.speech_v1p1beta1 import enums
     import io
 
-    # local_file_path = os.path.join("audio_samples", "acetest.wav")
-    local_file_path = os.path.join("audio_samples", "meeting_school_01_1mins.mp3")
+    file_path = "audio_samples/meeting_ct_02_1mins.ogg"
+    # file_path = "gs://cthack19-meeting-moderator-machine/audio_samples/meeting_business_04_2mins_channel1.ogg"
+    # file_path = "audio_samples/meeting_business_04_2mins.ogg"
+    # file_path = "audio_samples/acetest_ogg.wav"
+    # file_path = "audio_samples/meeting_school_01_1mins_mono.flac"
+    # local_file_path = os.path.join("audio_samples", "acetest.flac")
+    # local_file_path = os.path.join("audio_samples", "meeting_school_01_1mins.mp3")
+
+    if file_path.split(".")[-1] != "flac":
+        file_path = convert_ogg2flac(file_path)
 
     client = speech_v1p1beta1.SpeechClient(credentials=CREDENTIALS)
 
-    # If enabled, each word in the first alternative of each result will be
-    # tagged with a speaker tag to identify the speaker.
-    enable_speaker_diarization = True
-
-    # Optional. Specifies the estimated number of speakers in the conversation.
-    diarization_speaker_count = 2
-
-    # The language of the supplied audio
-    language_code = "en-US"
-    encoding = enums.RecognitionConfig.AudioEncoding.MP3
-
     config = {
-        "enable_speaker_diarization": enable_speaker_diarization,
-        "diarization_speaker_count": diarization_speaker_count,
-        "language_code": language_code,
-        "encoding": encoding,
+        "enable_speaker_diarization": True,
+        "diarization_speaker_count": 2,
+        # "audio_channel_count": 1,
+        "language_code": "en-US",
+        "encoding": enums.RecognitionConfig.AudioEncoding.FLAC,
+        # "sample_rate_hertz": 48000,
+        "max_alternatives": 1,
+        "use_enhanced": True
     }
-    with io.open(local_file_path, "rb") as f:
+
+    # Local file
+    with io.open(file_path, "rb") as f:
         content = f.read()
     audio = {"content": content}
+
+    # GCS Storage
+    # audio = {"uri": file_path}
 
     operation = client.long_running_recognize(config, audio)
 
     print(u"Waiting for operation to complete...")
     response = operation.result()
 
+    json_out = {}
     for result in response.results:
-        # First alternative has words tagged with speakers
         alternative = result.alternatives[0]
-        print(u"Transcript: {}".format(alternative.transcript))
-        # Print the speaker_tag of each word
+
+        json_out["raw_transcript"] = ' '.join([word.word for word in alternative.words])
+
+        transcript = []
+        sentence = []
+        last_speaker = alternative.words[0].speaker_tag
         for word in alternative.words:
-            print(u"Word: {}".format(word.word))
-            print(u"Speaker tag: {}".format(word.speaker_tag))
+            current_speaker = word.speaker_tag
+            if current_speaker == last_speaker:
+                sentence.append(word.word)
+            else:
+                transcript.append({
+                    "speaker_id": last_speaker,
+                    "line": ' '.join(sentence)
+                })
+                sentence = [word.word]
+                last_speaker = current_speaker
+        transcript.append("Speaker " + str(last_speaker) + ": " + ' '.join(sentence))
+        json_out["transcript"] = transcript
+
+        speaker_tags = [word.speaker_tag for word in alternative.words]
+        unique_speakers = set(speaker_tags)
+        speaker_ratios = []
+        for speaker in unique_speakers:
+            speaker_ratios.append({
+                "speaker_id": speaker,
+                "ratio": round(speaker_tags.count(speaker)/len(speaker_tags), 2)
+            })
+        json_out["speakers"] = speaker_ratios
+
+    pprint.pprint(json_out)
