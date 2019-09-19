defa0ca6409e995fc46697342b9dafca6e34ea34
Amadeus Magrabi <amadeus.magrabi@gmail.com>
2019-09-19 17:58:49 +0200

    Topic and sentiment analysis

--- analyze_speech.py
+++ analyze_speech.py
@@ -1,7 +1,7 @@
 import io
 
 from google.cloud import speech_v1p1beta1
-from google.cloud.speech_v1p1beta1 import enums
+from google.cloud import language_v1beta2
 from google.oauth2 import service_account
 from pydub import AudioSegment
 
@@ -14,6 +14,37 @@ def convert_ogg2flac(file_path):
     new_file_path = file_path.split(".")[0] + ".flac"
     audio.export(new_file_path, format="flac")
     return new_file_path
+
+
+def analyze_text(text):
+    """Takes a text and outputs statistics on sentiments and topics.
+
+        Args:
+            text (str)
+
+        Returns:
+            Dict[str, Any]
+
+        """
+    language_client = language_v1beta2.LanguageServiceClient(credentials=CREDENTIALS)
+    document = language_v1beta2.types.Document(
+        content=text,
+        type=language_v1beta2.enums.Document.Type.PLAIN_TEXT,
+        language="en"
+    )
+
+    # Analyze sentiment
+    sentiment = language_client.analyze_sentiment(document=document).document_sentiment
+    sentiment_out = {"score": round(sentiment.score, 2), "magnitude": round(sentiment.magnitude, 2)}
+
+    # Analyze topics
+    response = language_client.analyze_entities(document, encoding_type=language_v1beta2.enums.EncodingType.UTF8)
+    topics = []
+    for entity in response.entities:
+        topics.append({'topic': entity.name, 'ratio': round(entity.salience, 2)})
+    topics_out = sorted(topics, key=lambda k: k['ratio'], reverse=True)
+
+    return sentiment_out, topics_out
 
 
 def analyze_audio(file_path):
@@ -31,13 +62,13 @@ def analyze_audio(file_path):
     if file_path.split(".")[-1] != "flac":
         file_path = convert_ogg2flac(file_path)
 
-    client = speech_v1p1beta1.SpeechClient(credentials=CREDENTIALS)
+    speech_client = speech_v1p1beta1.SpeechClient(credentials=CREDENTIALS)
 
     config = {
         "enable_speaker_diarization": True,
         "diarization_speaker_count": 2,
         "language_code": "en-US",
-        "encoding": enums.RecognitionConfig.AudioEncoding.FLAC,
+        "encoding": speech_v1p1beta1.enums.RecognitionConfig.AudioEncoding.FLAC,
         "max_alternatives": 1,
         "use_enhanced": True
         # "audio_channel_count": 1,
@@ -52,45 +83,53 @@ def analyze_audio(file_path):
     # GCS Storage
     # audio = {"uri": file_path}
 
-    operation = client.long_running_recognize(config, audio)
+    operation = speech_client.long_running_recognize(config, audio)
     response = operation.result()
 
     json_out = {}
-    for result in response.results:
-        alternative = result.alternatives[0]
+    result = response.results[-1]
+    alternative = result.alternatives[0]
 
-        json_out["raw_transcript"] = ' '.join([word.word for word in alternative.words])
+    json_out["google_transcript"] = alternative.transcript
 
-        transcript = []
-        sentence = []
-        last_speaker = alternative.words[0].speaker_tag
-        for word in alternative.words:
-            current_speaker = word.speaker_tag
-            if current_speaker == last_speaker:
-                sentence.append(word.word)
-            else:
-                transcript.append({
-                    "speaker_id": last_speaker,
-                    "line": ' '.join(sentence)
-                })
-                sentence = [word.word]
-                last_speaker = current_speaker
-        transcript.append({
-                    "speaker_id": last_speaker,
-                    "line": ' '.join(sentence)
-                })
+    json_out["raw_transcript"] = ' '.join([word.word for word in alternative.words])
 
-        json_out["transcript"] = transcript
-
-        speaker_tags = [word.speaker_tag for word in alternative.words]
-        unique_speakers = set(speaker_tags)
-        speaker_ratios = []
-        for speaker in unique_speakers:
-            speaker_ratios.append({
-                "speaker_id": speaker,
-                "ratio": round(speaker_tags.count(speaker)/len(speaker_tags), 2)
+    # Get transcript distributed by speakers
+    transcript = []
+    sentence = []
+    last_speaker = alternative.words[0].speaker_tag
+    for word in alternative.words:
+        current_speaker = word.speaker_tag
+        if current_speaker == last_speaker:
+            sentence.append(word.word)
+        else:
+            transcript.append({
+                "speaker_id": last_speaker,
+                "line": ' '.join(sentence)
             })
-        json_out["speakers"] = speaker_ratios
+            sentence = [word.word]
+            last_speaker = current_speaker
+    transcript.append({
+                "speaker_id": last_speaker,
+                "line": ' '.join(sentence)
+            })
+    json_out["transcript"] = transcript
+
+    # Analyze speakers
+    speaker_tags = [word.speaker_tag for word in alternative.words]
+    unique_speakers = set(speaker_tags)
+    speaker_ratios = []
+    for speaker in unique_speakers:
+        speaker_ratios.append({
+            "speaker_id": speaker,
+            "ratio": round(speaker_tags.count(speaker)/len(speaker_tags), 2)
+        })
+    json_out["speakers"] = speaker_ratios
+
+    # Analyze sentiment and topics
+    sentiment, topics = analyze_text(json_out["raw_transcript"])
+    json_out["sentiment"] = sentiment
+    json_out["topics"] = topics
 
     return json_out
 
