c90b0ac539457e691f050cfdb98795fe0d40784f
Islam ezzat <islam3zzat@gmail.com>
2019-09-20 12:03:07 +0200

    reset colors

--- templates/precache-manifest.9c82557342ff54a5ae13d5a59126dbe6.js
+++ templates/precache-manifest.9c82557342ff54a5ae13d5a59126dbe6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d11da7ad47e916b1f302d822a65664ee",
+    "revision": "7096f5cca7ed6f23c7b7e658bda67fd4",
     "url": "/index.html"
   },
   {
-    "revision": "64857b41be3fb45f9d4a",
+    "revision": "ca3feac53f5fffde8800",
     "url": "/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "aa5d2fd7674fdef7e6de",
-    "url": "/static/js/2.bce22bc7.chunk.js"
+    "revision": "e43ae3a3d4e1b0a52179",
+    "url": "/static/js/2.ff1f0385.chunk.js"
   },
   {
-    "revision": "64857b41be3fb45f9d4a",
-    "url": "/static/js/main.4d0afaf8.chunk.js"
+    "revision": "ca3feac53f5fffde8800",
+    "url": "/static/js/main.5d4c15a4.chunk.js"
   },
   {
     "revision": "8fd597a5f82a416e489b",
