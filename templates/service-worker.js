a113c41389f157e44a35198e9d84e9addaad3b8e
Islam ezzat <islam3zzat@gmail.com>
2019-09-20 11:59:42 +0200

    use other colors

--- templates/service-worker.js
+++ templates/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c82557342ff54a5ae13d5a59126dbe6.js"
+  "/precache-manifest.00a28d7775984eae6784cfb4b55282da.js"
 );
 
 self.addEventListener('message', (event) => {
